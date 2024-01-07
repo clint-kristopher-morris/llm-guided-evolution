639687977e1f9a8e6ecc20b917f794f322f981c5
Jason Zutty <jasonzutty@gmail.com>
2024-01-07 11:30:32 -0500

    trying to get a local version working, incremental steps.

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem 8G
 #SBATCH -c 1                          # number of CPU cores
-echo "launching AIsurBL"
-hostname
-# module load anaconda3/2020.07 2021.11
-module load cuda/11.0
-export CUDA_VISIBLE_DEVICES=0
+# echo "launching AIsurBL"
+# hostname
+# # module load anaconda3/2020.07 2021.11
+# module load cuda/11.0
+# export CUDA_VISIBLE_DEVICES=0
 
-source /opt/apps/Module/anaconda3/2021.11/bin/activate
-conda activate AIsurBL
-conda info
+# source /opt/apps/Module/anaconda3/2021.11/bin/activate
+# conda activate AIsurBL
+# conda info
 
 python run.py

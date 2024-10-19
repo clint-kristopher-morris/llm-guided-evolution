e973489b2858b71df038bc603ffeb4fce436969a
Jason Paul Zutty <jzutty3@atl1-1-03-010-30-0.pace.gatech.edu>
2024-10-19 15:24:59 -0400

    Changes made to support pace-ice

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=llm_opt
 #SBATCH -t 8-00:00              		# Runtime in D-HH:MM
-#SBATCH --mem 16G
-#SBATCH -c 1                          # number of CPU cores
-#SBATCH -G 1
+#SBATCH --mem-per-gpu 16G
+#SBATCH -n 1                          # number of CPU cores
+#SBATCH -N 1
 #SBATCH --gres=gpu:1
-#SBATCH -C "NVIDIAA100-SXM4-80GB|NVIDIAA10080GBPCIe|TeslaV100-PCIE-32GB|TeslaV100S-PCIE-32GB|NVIDIARTX6000AdaGeneration|NVIDIARTXA6000|NVIDIARTXA5000|NVIDIARTXA4000|GeForceGTX1080Ti|QuadroRTX4000|QuadroP4000|GeForceGTX1080|TeslaP4"
-echo "launching AIsurBL"
+#SBATCH -C "A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S"
+echo "launching LLM Guided Evolution"
 hostname
 # module load anaconda3/2020.07 2021.11
-module load cuda/12
+module load cuda
+module load anaconda3
 export CUDA_VISIBLE_DEVICES=0
 
-source /opt/apps/Module/anaconda3/2021.11/bin/activate
-conda activate llm_guided_evolution
+conda activate llm_guided_env
+export LD_LIBRARY_PATH=~/.conda/envs/llm_guided_env/lib/python3.12/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
 conda info
 
 python run_improved.py first_test

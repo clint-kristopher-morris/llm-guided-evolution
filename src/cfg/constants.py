81d9ab4ed61a8d71da34947cc5b362a79ce3548c
Jason Paul Zutty <jzutty3@atl1-1-03-010-30-0.pace.gatech.edu>
2024-10-19 15:24:59 -0400

    Changes made to support pace-ice

--- src/cfg/constants.py
+++ src/cfg/constants.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 
-ROOT_DIR = "/home/jzutty3/llm-guided-evolution"
+ROOT_DIR = "/home/hice1/jzutty3/llm-guided-evolution"
 # DATA_PATH absolute or relative to ExquisiteNetV2
 DATA_PATH = "./cifar10"
 SOTA_ROOT = os.path.join(ROOT_DIR, 'sota/ExquisiteNetV2')
@@ -53,24 +53,26 @@ Job Sub Constants/Params
 QC_CHECK_BOOL = False
 HUGGING_FACE_BOOL = True
 #LLM_GPU = 'NVIDIAA100-SXM4-80GB|NVIDIAA10080GBPCIe|TeslaV100-PCIE-32GB|QuadroRTX4000|GeForceGTX1080Ti|GeForceGTX1080|TeslaV100-PCIE-32GB|TeslaV100S-PCIE-32GB'
-LLM_GPU = 'NVIDIAA100-SXM4-80GB|NVIDIAA10080GBPCIe|TeslaV100-PCIE-32GB|TeslaV100S-PCIE-32GB|NVIDIARTX6000AdaGeneration|NVIDIARTXA6000|NVIDIARTXA5000|NVIDIARTXA4000|GeForceGTX1080Ti|QuadroRTX4000|QuadroP4000|GeForceGTX1080|TeslaP4'
+#LLM_GPU = 'NVIDIAA100-SXM4-80GB|NVIDIAA10080GBPCIe|TeslaV100-PCIE-32GB|TeslaV100S-PCIE-32GB|NVIDIARTX6000AdaGeneration|NVIDIARTXA6000|NVIDIARTXA5000|NVIDIARTXA4000|GeForceGTX1080Ti|QuadroRTX4000|QuadroP4000|GeForceGTX1080|TeslaP4'
+LLM_GPU = 'A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S'
 PYTHON_BASH_SCRIPT_TEMPLATE = """#!/bin/bash
 #SBATCH --job-name=evaluateGene
-#SBATCH -t 8-00:00
+#SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
-#SBATCH -G 1
-#SBATCH -C "NVIDIAA100-SXM4-80GB|NVIDIAA10080GBPCIe|TeslaV100-PCIE-32GB|TeslaV100S-PCIE-32GB|NVIDIARTX6000AdaGeneration|NVIDIARTXA6000|NVIDIARTXA5000|NVIDIARTXA4000|GeForceGTX1080Ti"
-#SBATCH --mem 16G
-#SBATCH -c 12
-echo "Launching AIsurBL"
+#SBATCH -C "A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S"
+#SBATCH --mem-per-gpu 16G
+#SBATCH -n 12
+#SBATCH -N 1
+echo "Launching Python Evaluation"
 hostname
 
 # Load GCC version 9.2.0
 # module load gcc/13.2.0
-module load cuda/12
-
+module load cuda
+module load anaconda3
 # Activate Conda environment
-source /opt/apps/Module/anaconda3/2021.11/bin/activate llm_guided_evolution
+conda activate llm_guided_env
+export LD_LIBRARY_PATH=~/.conda/envs/llm_guided_env/lib/python3.12/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
 # conda info
 
 # Set the TOKENIZERS_PARALLELISM environment variable if needed
@@ -82,21 +84,23 @@ source /opt/apps/Module/anaconda3/2021.11/bin/activate llm_guided_evolution
 
 LLM_BASH_SCRIPT_TEMPLATE = """#!/bin/bash
 #SBATCH --job-name=llm_oper
-#SBATCH -t 8-00:00
+#SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
-#SBATCH -G 1
 #SBATCH -C "{}"
-#SBATCH --mem 16G
-#SBATCH -c 12
+#SBATCH --mem-per-gpu 16G
+#SBATCH -n 12
+#SBATCH -N 1
 echo "Launching AIsurBL"
 hostname
 
 # Load GCC version 9.2.0
 # module load gcc/13.2.0
 # module load cuda/11.8
-module load cuda/12
+module load cuda
+module load anaconda3
 # Activate Conda environment
-source /opt/apps/Module/anaconda3/2021.11/bin/activate llm_guided_evolution
+conda activate llm_guided_env
+export LD_LIBRARY_PATH=~/.conda/envs/llm_guided_env/lib/python3.12/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
 # conda info
 
 # Set the TOKENIZERS_PARALLELISM environment variable if needed

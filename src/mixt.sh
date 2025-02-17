9da0330f29dfabd1d05cbecdd60aadd307106cd1
Moses Adewolu <69429336+MosesTheRedSea@users.noreply.github.com>
2025-02-17 18:02:51 -0500

    Update mixt.sh

--- src/mixt.sh
+++ src/mixt.sh
@@ -1,7 +1,3 @@
-#________________________________________________________________________
-# Changed indivudal configrations like path for main model in Point Transformers #1
-#________________________________________________________________________
-
 #!/bin/bash
 #SBATCH --job-name=AIsur_x1
 #SBATCH -t 8-00:00
@@ -24,16 +20,4 @@ conda info
 # Set the TOKENIZERS_PARALLELISM environment variable if needed
 export TOKENIZERS_PARALLELISM=false
 
-#________________________________________________________________________
-# Added Run Scripts Commands to run PointNet++ using PyTorch it reflects my Path update yours as well
-# Added Run Scripts COmmands to run Point Transformers using 
-#________________________________________________________________________
-
-# Run Python script - ExquisiteNetV2
 python llm_crossover.py '/gv1/projects/AI_Surrogate/dev/clint/CodeLLama/codellama/sota/ExquisiteNetV2/network.py' '/gv1/projects/AI_Surrogate/dev/clint/CodeLLama/codellama/sota/ExquisiteNetV2/models/network_x.py' '/gv1/projects/AI_Surrogate/dev/clint/CodeLLama/codellama/sota/ExquisiteNetV2/models/network_z.py' --top_p 0.15 --temperature 0.1 --apply_quality_control 'True' --bit 8
-
-# Run Python Script - PointNet++ PyTorch #1
-
-
-# Run python Script - Point Transformers #1
-#python llm_crossover.py '/gv1/projects/AI_Surrogate/dev/dev/clint/CodeLLama/codellama/sota/Point-Transformers/models/Menghao/model.py' '/home/hice1/madewolu9/scratch/madewolu9/LLM_PointNet/LLM-Guided-PointCloud-Class/sota/Point-Transformers/models/llmge_models/model_x.py' '/home/hice1/madewolu9/scratch/madewolu9/LLM_PointNet/LLM-Guided-PointCloud-Class/sota/Point-Transformers/models/llmge_models/model_z.py'  --top_p 0.15 --temperature 0.1 --apply_quality_control 'True' --bit 8

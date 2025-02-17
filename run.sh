4758865bce93c1dce1680fb72e2d0dbbce31dbed
Moses Adewolu <69429336+MosesTheRedSea@users.noreply.github.com>
2025-02-17 18:01:20 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
-"""
-█▀▀ █──█ █▀▀█ █▀▀▄ █▀▀▀ █▀▀ █▀▀ 
-█── █▀▀█ █▄▄█ █──█ █─▀█ █▀▀ ▀▀█ 
-▀▀▀ ▀──▀ ▀──▀ ▀──▀ ▀▀▀▀ ▀▀▀ ▀▀▀
-"""
-#________________________________________________________________________
-# Alternative Configurations for clean run on PACE ICE #1
-# Changed indivudal configrations conda enviroment etc #2
-#________________________________________________________________________
-
 #!/bin/bash
 #SBATCH --job-name=llm_opt
 #SBATCH -t 8:00:00              		# Runtime in D-HH:MM
@@ -16,14 +6,14 @@
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
 #SBATCH -C "A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S"
-echo "launching LLM Guided Evolution" #1
-hostname #1
+echo "launching LLM Guided Evolution" 
+hostname 
 # module load anaconda3/2020.07 2021.11 
-module load cuda/12 #1
-module load cuda #1 
-module load anaconda3 #1
+module load cuda/12 
+module load cuda 
+module load anaconda3 
 #export CUDA_VISIBLE_DEVICES=0
 export MKL_THREADING_LAYER=GNU # Had to Change 
-conda activate llmge-env  #2
+conda activate llmge-env  
 conda info 
 python run_improved.py first_test
